84975660586a9729f32399e61d43dbc8e2dc6bdd
Camilo Cerda Vasquez <c.cerda@duocuc.cl>
2021-04-11 13:26:59 -0400

    se creo la pagina para buscar jugadores, se hizo adaptable a dispositivos moviles y se enlazó con la pagina principal

--- javascript/nuestrosJugadores.js
+++ javascript/nuestrosJugadores.js
@@ -11,17 +11,41 @@ class jugador {
 }
 
 let jugadores = [];
-jugadores.push(new jugador(1, 'Camilo Cerda', 28, 'estilo 1', 'T.C.', 'hombre', 'descripcion'));
-jugadores.push(new jugador(2, 'Gonzalo Muñoz', 31, 'estilo 2', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(1, 'Camilo Cerda', 28, 'clásico', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(2, 'Gonzalo Muñoz', 31, 'clásico', 'T.C.', 'hombre', 'descripcion'));
 jugadores.push(new jugador(3, 'Claudio Palacios', 27, 'estilo 3', 'T.C.', 'hombre', 'descripcion'));
-jugadores.push(new jugador(4, 'Camilo Masias', 16, 'estilo 3', 'juvenil', 'hombre', 'descripcion'));
-jugadores.push(new jugador(5, 'Ding Ning', 18, 'estilo 3', 'juvenil', 'mujer', 'descripcion'));
+jugadores.push(new jugador(4, 'Camilo Masias', 16, 'clásico', 'juvenil', 'hombre', 'descripcion'));
+jugadores.push(new jugador(5, 'Ding Ning', 18, 'clásico', 'juvenil', 'mujer', 'descripcion'));
+jugadores.push(new jugador(6, 'Pablo Duffe', 26, 'clásico', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(7, 'Ivan Sánchez', 22, 'clásico', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(8, 'Rodrigo Correa', 35, 'clásico', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(9, 'Claudio Verdugo', 26, 'clásico', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(10, 'Miguel Quilodrán', 27, 'lapicero', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(11, 'Bastián Maureira', 25, 'lapicero', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(12, 'Matias Arias', 26, 'lapicero', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(13, 'Javier Herdocio', 55, 'lapicero', 'T.C.', 'hombre', 'descripcion'));
+jugadores.push(new jugador(14, 'Bernadette Sockz', 27, 'clásico', 'T.C.', 'mujer', 'descripcion'));
+jugadores.push(new jugador(15, 'Mima Ito', 20, 'clásico', 'T.C.', 'mujer', 'descripcion'));
+jugadores.push(new jugador(16, 'Liu Shiwen', 24, 'clásico', 'T.C.', 'mujer', 'descripcion'));
+jugadores.push(new jugador(17, 'Chen Meng', 18, 'clásico', 'juvenil', 'mujer', 'descripcion'));
 
-let imagenesJugadores = [[1, `C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/img/tdm/galeria/jugadores/9.jpg`],
-                         [2, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/img/tdm/galeria/jugadores/6.jpg'], 
-                         [3, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/img/tdm/galeria/jugadores/2.jpg'],
-                         [4, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/img/tdm/galeria/jugadores/1.jpg'], 
-                         [5, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/img/tdm/galeria/ding-ning.jpg']];
+let imagenesJugadores = [[1, `C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/9.jpg`],
+                         [2, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/6.jpg'], 
+                         [3, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/2.jpg'],
+                         [4, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/1.jpg'], 
+                         [5, `C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/13.jpg`],
+                         [6, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/3.jpg'], 
+                         [7, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/5.jpg'],
+                         [8, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/4.jpg'],
+                         [9, `C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/10.jpg`],
+                         [10, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/12.jpg'], 
+                         [11, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/11.jpg'],
+                         [12, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/8.jpg'],
+                         [13, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/7.jpg'],
+                         [14, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/14.jpg'], 
+                         [15, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/15.jpg'],
+                         [16, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/16.jpg'],
+                         [17, 'C:/Users/Camilo Cerda Vasquez/Desktop/Paginas de prueba/Javascript/Repositorios git/repositorio entregable 1/img/tdm/galeria/jugadores/17.jpg']];
                      
 function buscar(){
     let radioSeleccionado, comboBoxCategoriaSeleccionado; 
@@ -76,8 +100,6 @@ function buscar(){
             jugadorDiv.appendChild(nombreJugador);
         }
     }
-    //console.log(jugadoresCumplenSeleccion);
-    //alert(radioSeleccionado+' y '+ comboBoxCategoriaSeleccionado)
 }
 /*--- Variables ---*/
 var botonBuscar = document.querySelector('.buscar'),
