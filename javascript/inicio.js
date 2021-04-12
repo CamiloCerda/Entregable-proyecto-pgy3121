30edb0d5b1ff0c34d5611d8aae921e77d07087c4
Camilo Cerda Vasquez <c.cerda@duocuc.cl>
2021-04-12 17:00:54 -0400

    se creo el formulario de registro como ventana modal, se modifico el btn invitado con js, se ajustaron algunos links

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -1,8 +1,20 @@
 let sonidopingpong = document.querySelector('.sonidopingpong');
 let inputUserName = document.querySelector('.nombreusuario');
+let acceder = document.querySelector('.acceder');
+
 
 function sonar() {
     sonidopingpong.play();
 }
 
 inputUserName.addEventListener('click', sonar);
+
+/* function entrar(){
+    window.open('../html/principal.html', '_self');
+}
+acceder.addEventListener('click', entrar); */
+
+acceder.addEventListener('click', function(){
+    window.open('../html/principal.html', '_self');
+});
+
