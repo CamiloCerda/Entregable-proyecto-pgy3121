6a15d651eae7bc5300e8183068e8c98bbefb0148
Camilo Cerda Vasquez <c.cerda@duocuc.cl>
2021-04-19 19:21:42 -0400

    se creo la seccion que muestra una galeria del centro de entrenamiento y se enlazo con cada pagina dentro del sitio

--- javascript/inicio.js
+++ javascript/inicio.js
@@ -2,6 +2,12 @@ let sonidopingpong = document.querySelector('.sonidopingpong');
 let inputUserName = document.querySelector('.nombreusuario');
 let acceder = document.querySelector('.acceder');
 
+let ingresar = document.querySelector('.ingresar');
+let alerta = document.querySelector('.alert');
+
+ingresar.addEventListener('click', function(){
+    alerta.classList.replace('d-none', 'd-block');
+});
 
 function sonar() {
     sonidopingpong.play();
